f53efe1ee97d39d4f7d3ff904169b7751de79bc7
ldalzotto <loic.dalzotto@hotmail.fr>
2020-08-05 16:29:42 +0200

    Adding additional input key handling for windows

--- GameEngine/Input/InputGlobalEvent.cpp
+++ GameEngine/Input/InputGlobalEvent.cpp
@@ -41,8 +41,139 @@ namespace _GameEngine::_Input
 			InputGlobalEvent l_inputGlobalEvent;
 			l_inputGlobalEvent.Type = static_cast<InputGlobalEventType>(l_inputKeyEvent->Type);
 
+			//see https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 			switch (l_inputKeyEvent->Key)
 			{
+			case 0x41:
+			{
+				l_inputGlobalEvent.Key = InputKey::A;
+			}
+			break;
+			case 0x42:
+			{
+				l_inputGlobalEvent.Key = InputKey::B;
+			}
+			break;
+			case 0x43:
+			{
+				l_inputGlobalEvent.Key = InputKey::C;
+			}
+			break;
+			case 0x44:
+			{
+				l_inputGlobalEvent.Key = InputKey::D;
+			}
+			break;
+			case 0x45:
+			{
+				l_inputGlobalEvent.Key = InputKey::E;
+			}
+			break;
+			case 0x46:
+			{
+				l_inputGlobalEvent.Key = InputKey::F;
+			}
+			break;
+			case 0x47:
+			{
+				l_inputGlobalEvent.Key = InputKey::G;
+			}
+			break;
+			case 0x48:
+			{
+				l_inputGlobalEvent.Key = InputKey::H;
+			}
+			break;
+			case 0x49:
+			{
+				l_inputGlobalEvent.Key = InputKey::I;
+			}
+			break;
+			case 0x4A:
+			{
+				l_inputGlobalEvent.Key = InputKey::J;
+			}
+			break;
+			case 0x4B:
+			{
+				l_inputGlobalEvent.Key = InputKey::K;
+			}
+			break;
+			case 0x4C:
+			{
+				l_inputGlobalEvent.Key = InputKey::L;
+			}
+			break;
+			case 0x4D:
+			{
+				l_inputGlobalEvent.Key = InputKey::M;
+			}
+			break;
+			case 0x4E:
+			{
+				l_inputGlobalEvent.Key = InputKey::N;
+			}
+			break;
+			case 0x4F:
+			{
+				l_inputGlobalEvent.Key = InputKey::O;
+			}
+			break;
+			case 0x50:
+			{
+				l_inputGlobalEvent.Key = InputKey::P;
+			}
+			break;
+			case 0x51:
+			{
+				l_inputGlobalEvent.Key = InputKey::Q;
+			}
+			break;
+			case 0x52:
+			{
+				l_inputGlobalEvent.Key = InputKey::R;
+			}
+			break;
+			case 0x53:
+			{
+				l_inputGlobalEvent.Key = InputKey::S;
+			}
+			break;
+			case 0x54:
+			{
+				l_inputGlobalEvent.Key = InputKey::T;
+			}
+			break;
+			case 0x55:
+			{
+				l_inputGlobalEvent.Key = InputKey::U;
+			}
+			break;
+			case 0x56:
+			{
+				l_inputGlobalEvent.Key = InputKey::V;
+			}
+			break;
+			case 0x57:
+			{
+				l_inputGlobalEvent.Key = InputKey::W;
+			}
+			break;
+			case 0x58:
+			{
+				l_inputGlobalEvent.Key = InputKey::X;
+			}
+			break;
+			case 0x59:
+			{
+				l_inputGlobalEvent.Key = InputKey::Y;
+			}
+			break;
+			case 0x5A:
+			{
+				l_inputGlobalEvent.Key = InputKey::Z;
+			}
+			break;
 			case WM_LBUTTONDOWN:
 			{
 				l_inputGlobalEvent.Key = InputKey::MOUSE_BUTTON_1;
@@ -63,7 +194,7 @@ namespace _GameEngine::_Input
 		{
 			_Core::InputMouseEvent* l_inputMouseEvent = (_Core::InputMouseEvent*)p_eventHeader;
 			InputMouseGlobalEvent l_inputMouseGlobalEvent;
-			l_inputMouseGlobalEvent.X = l_inputMouseEvent->X; 
+			l_inputMouseGlobalEvent.X = l_inputMouseEvent->X;
 			l_inputMouseGlobalEvent.Y = l_inputMouseEvent->Y;
 
 			_Core::ObserverT_broadcast(&InputMouseGlobalEvent_dispatcher, &l_inputMouseGlobalEvent);
