5c03a6b3733b83cbd964bf189e3d95bad4167519
ldalzotto <loic.dalzotto@hotmail.fr>
2020-08-16 14:04:53 +0200

    REFACTOR - No memory alloc for the test

--- Render/Objects/Window/Window.cpp
+++ Render/Objects/Window/Window.cpp
@@ -18,8 +18,8 @@ namespace _RenderV2
 	void windowPlatformSpecific_paintTexture(Window* p_window);
 	void windowPlatforwSpecific_getClientRect(Window* p_windout, int* out_width, int* out_height);
 
-	const uint32_t WINDOW_WIDTH = 1584;
-	const uint32_t WINDOW_HEIGHT = 1061;
+	const uint32_t WINDOW_WIDTH = 800;
+	const uint32_t WINDOW_HEIGHT = 600;
 
 	const char* WINDOW_ERROR_NOT_INITIALIZED = "The Window->Window is not initialized.";
 
--- Test/src/MyRenderTest.cpp
+++ Test/src/MyRenderTest.cpp
@@ -6,6 +6,7 @@
 #include "AppEvent/AppEvent.hpp"
 #include "DataStructures/Specifications/VectorT.hpp"
 
+#include "v2/Math.h"
 #include "v2/Matrix/MatrixMath.hpp"
 #include "v2/Quaternion/QuaternionMath.hpp"
 #include "v2/Vector/VectorMath.hpp"
@@ -38,19 +39,22 @@ int main(int argc, char* argv[])
 	// l_modelMatrix.Points[3][0] = 9.0f;
 
 	MeshResourceKey l_meshResourceKey;
-	_Core::String_alloc(&l_meshResourceKey.MeshPathAbsolute, 0); _Core::String_append(&l_meshResourceKey.MeshPathAbsolute, "E:/GameProjects/GameEngine/Assets/Models/ForwardArrow.obj");
+	_Core::String_alloc(&l_meshResourceKey.MeshPathAbsolute, 0); _Core::String_append(&l_meshResourceKey.MeshPathAbsolute, "C:/Users/loicd/Desktop/BigCube.obj");
 	MeshResource* l_mesh = _Core::ResourceProviderT_useResource(&renderV2.Resources.MeshResourceProvider, &l_meshResourceKey, MeshResourceKey_getHashCode(&l_meshResourceKey));
-	RenderedObject* l_renderableObject = new RenderedObject();
+	RenderedObject l_renderableObject;
+	RenderedObject* l_renderableObject_ptr = &l_renderableObject;
 	_MathV2::Box l_meshBoundingBox{};
 	{
 		_MathV2::Box_build(&l_meshBoundingBox, (_Core::VectorT<_MathV2::Vector3<float>>*) & l_mesh->Mesh.Vertices);
-		*l_renderableObject = { &l_mesh->Mesh , &l_meshBoundingBox, l_modelMatrix };
-		_Core::VectorT_pushBack(&renderV2.GlobalBuffer.RenderedObjectsBuffer.RenderedObjects, &l_renderableObject);
+		l_renderableObject = { &l_mesh->Mesh , &l_meshBoundingBox, l_modelMatrix };
+		_Core::VectorT_pushBack(&renderV2.GlobalBuffer.RenderedObjectsBuffer.RenderedObjects, &l_renderableObject_ptr);
 	}
 
 	_MathV2::Matrix<4, 4, float> l_viewMatrix = { 0.7071f, 0.4156f, 0.5720f, 0.00f, 0.00f, -0.8090f, 0.5877f, -0.00f, -0.7071f, 0.4156f, 0.5720f, 0.00f, 0.00f, -0.2001f, -15.5871f, 1.00f };
-	// _MathV2::Matrix<4, 4, float> l_projectionMatrix = { 1.7275f, 0.00f, 0.00f, 0.00f, 0.00f, 2.4142f, 0.00f, 0.00f, 0.00f, 0.00f, (1.0f / 1.0040f), -1.0000f, 0.00f, 0.00f, -0.2004f, 0.00f };
-	_MathV2::Matrix<4, 4, float> l_projectionMatrix = { 1.5088f, 0.00f, 0.00f, 0.00f, 0.00f, 2.4142f, 0.00f, 0.00f, 0.00f, 0.00f, (1.0f / 1.0040f), -1.0000f, 0.00f, 0.00f, -0.2004f, 0.00f };
+	_MathV2::Matrix<4, 4, float> l_projectionMatrix;
+	_MathV2::MatrixM::perspective(45.0f * _MathV2::DEG_TO_RAD,
+		((float)renderV2.SwapChain.PresentTexture.Width / (float)renderV2.SwapChain.PresentTexture.Height), 0.1f, 50.0f, &l_projectionMatrix);
+		
 	_MathV2::Vector<4, float> l_cameraWorldPosition = { 9.0f, 9.0f, 9.0f, 1.0f };
 	_MathV2::Frustum l_cameraFrustum = _MathV2::FrustumM::extractFrustumFromProjection(&l_projectionMatrix);
 
@@ -71,8 +75,7 @@ int main(int argc, char* argv[])
 		_MathV2::Quaternion<float> tmp_quat_0; _MathV2::Matrix3x3<float> tmp_mat3x3_0; _MathV2::Matrix4x4<float> tmp_mat4x4_0;
 		_MathV2::Matrix4x4<float> l_rotation = _MathV2::Matrix4x4f_Identity;
 		_MathV2::MatrixM::buildRotationMatrix(_MathV2::QuaternionM::extractAxis(_MathV2::QuaternionM::rotateAround(&_MathV2::UP, 0.000001f * l_deltaTime, &tmp_quat_0), &tmp_mat3x3_0), &l_rotation);
-		_MathV2::MatrixM::mul(&l_modelMatrix, &l_rotation, &tmp_mat4x4_0);
-		l_modelMatrix = tmp_mat4x4_0;
+		l_renderableObject.ModelMatrix = *_MathV2::MatrixM::mul(&l_renderableObject.ModelMatrix, &l_rotation, &tmp_mat4x4_0);
 
 		RenderV2_render(&renderV2);
 	}
