e80232fa46c5a0d1f048e5f9f5700332d7ff5111
ldalzotto <loic.dalzotto@hotmail.fr>
2020-09-16 03:54:57 +0200

    FIX - Fixing camera on MyRender test

--- Test/src/MyRenderTest.c
+++ Test/src/MyRenderTest.c
@@ -53,18 +53,10 @@ int main(int argc, char* argv[])
 	l_renderableObject = (RenderedObject){ .Mesh = &l_mesh->Mesh , .MeshBoundingBox = &l_meshBoundingBox, .ModelMatrix = l_modelMatrix, .Material = l_materialHandle };
 	Arr_PushBackRealloc_RenderedObjectHandle(&renderV2.GlobalBuffer.RenderedObjectsBuffer.RenderedObjects, &l_renderableObject_ptr);
 
-	Matrix4f l_viewMatrix = { 0.7071f, 0.4156f, 0.5720f, 0.00f, 0.00f, -0.8090f, 0.5877f, -0.00f, -0.7071f, 0.4156f, 0.5720f, 0.00f, 0.00f, -0.2001f, -15.5871f, 1.00f };
-	{
-		Matrix4f tmp_mat4_0;
-		Vector3f l_source = { 9.0f, 9.0f, 9.0f }; Vector3f l_target = (Vector3f){ 0.0f, 0.0f,0.0f };
-		Vector3f l_one = { 1.0f, 1.0f, 1.0f };
-		Matrix3f l_axis;
-		Vector3f l_down = { 0.0f, -1.0f, 0.0f };
-		Mat_LookAtRotation_F(&l_source, &l_target, &l_down, &l_axis);
+	Vector3f l_cameraPos = { 9.0f, 9.0f, 9.0f }; Vector3f l_forward = { -0.572061539f, -0.587785244f, -0.572061360f }; Vector3f l_up = { -0.415627033f, 0.809017003f, -0.415626884f };
+	Matrix4f l_viewMatrix;
+	Mat_ViewMatrix_M4F(&l_cameraPos, &l_forward, &l_up, &l_viewMatrix);
 
-		Mat_TRS_Axis_M4F(&l_source, &l_axis, &l_one, &tmp_mat4_0);
-		Mat_Inv_M4F(&tmp_mat4_0, &l_viewMatrix);
-	}
 	Matrix4f l_projectionMatrix;
 	Mat_Perspective_M4F(45.0f * DEG_TO_RAD,
 		((float)renderV2.SwapChain.PresentTexture.Width / (float)renderV2.SwapChain.PresentTexture.Height), 0.1f, 50.0f, &l_projectionMatrix);
