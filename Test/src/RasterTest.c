dcc6cbfaa7b5dc6745f76e17a557cc8d5088735d
ldalzotto <loic.dalzotto@hotmail.fr>
2020-09-22 17:19:43 +0200

    FIX - Wrong recursive call in the interpolated rasterizer

--- Test/src/RasterTest.c
+++ Test/src/RasterTest.c
@@ -19,11 +19,11 @@ int main()
 {
 	Polygon2i l_polygon =
 	{
-		.v1 = {0,0},
-		.v2 = {6, 2},
-		.v3 = {2, 6}
+		.v1 = {59,90},
+		.v2 = {40, 91},
+		.v3 = {44, 106}
 	};
-	Recti l_rect = { .Min = {0,0}, .Max = {100,100} };
+	Recti l_rect = { .Min = {0,0}, .Max = {319,239} };
 	Array_Vector2i l_pixels;
 	Arr_Alloc_Vector2i(&l_pixels, 100 * 100);
 	Array_TestInterp l_interpolations;
@@ -32,20 +32,21 @@ int main()
 	//Rasterize_PolygonClipped
 	// Rasterize_PolygonClipped(&l_polygon, &l_pixels, &l_rect);
 	//Draw_PolygonClipped(&l_polygon, );
-
+	 
 	Arr_Clear(&l_pixels.array);
 	PolygonRasterizerIterator l_rasterizerIterator;
-	PolygonRasterize_Initialize(&l_polygon, &l_rect, 1, &l_rasterizerIterator);
+	PolygonRasterize_Initialize(&l_polygon, &l_rect, &l_rasterizerIterator);
 	POLYGONRASTERIZER_ITERATOR_RETURN_CODE l_returnCode = POLYGONRASTERIZER_ITERATOR_RETURN_CODE_PIXEL_NOT_RASTERIZED;
 	while (l_returnCode != POLYGONRASTERIZER_ITERATOR_RETURN_CODE_END)
 	{
-		l_returnCode = PolygonRasterize_MoveNext(&l_rasterizerIterator);
+		l_returnCode = PolygonRasterize_MoveNext_Interpolated(&l_rasterizerIterator);
 		if (l_returnCode == POLYGONRASTERIZER_ITERATOR_RETURN_CODE_PIXEL_RASTERIZED)
 		{
-			Arr_PushBackRealloc_Vector2i(&l_pixels, &l_rasterizerIterator.RasterizedPixel);
-
-			TestInterp l_interp = {l_rasterizerIterator.I0, l_rasterizerIterator.I1, l_rasterizerIterator.I2 };
-			Arr_PushBackRealloc_TestInterp(&l_interpolations, &l_interp);
+			int l_debug = 10;
+			// Arr_PushBackRealloc_Vector2i(&l_pixels, &l_rasterizerIterator.RasterizedPixel);
+			// 
+			// TestInterp l_interp = {l_rasterizerIterator.I0, l_rasterizerIterator.I1, l_rasterizerIterator.I2 };
+			// Arr_PushBackRealloc_TestInterp(&l_interpolations, &l_interp);
 		}
 	}
 	return 0;
