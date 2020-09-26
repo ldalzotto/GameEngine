8252fe286b50229be94e409bd9984c62c6f68b4f
ldalzotto <loic.dalzotto@hotmail.fr>
2020-09-26 06:14:44 +0200

    REFACTOR - Trying intrinsic for math operation

--- Math/v2/_source/Math.c
+++ Math/v2/_source/Math.c
@@ -1,5 +1,7 @@
 ﻿#include "v2/Math.h"
 
+#include <intrin.h>
+
 #include "v2/_inline/Math_inline.c"
 #include "Functional/Equals/Equals.h"
 #include "v2/_interface/VectorC.h"
@@ -681,30 +683,35 @@ inline float Mat_Det_M4F(const Matrix4f_PTR p_mat, const short int p_colIndex, c
 
 void Mat_Mul_M4F_M4F(const Matrix4f_PTR p_left, const Matrix4f_PTR p_right, Matrix4f_PTR p_out)
 {
-	p_out->_00 = (p_left->_00 * p_right->_00) + (p_left->_10 * p_right->_01) + (p_left->_20 * p_right->_02) + (p_left->_30 * p_right->_03);
-	p_out->_01 = (p_left->_01 * p_right->_00) + (p_left->_11 * p_right->_01) + (p_left->_21 * p_right->_02) + (p_left->_31 * p_right->_03);
-	p_out->_02 = (p_left->_02 * p_right->_00) + (p_left->_12 * p_right->_01) + (p_left->_22 * p_right->_02) + (p_left->_32 * p_right->_03);
-	p_out->_03 = (p_left->_03 * p_right->_00) + (p_left->_13 * p_right->_01) + (p_left->_23 * p_right->_02) + (p_left->_33 * p_right->_03);
-
-	p_out->_10 = (p_left->_00 * p_right->_10) + (p_left->_10 * p_right->_11) + (p_left->_20 * p_right->_12) + (p_left->_30 * p_right->_13);
-	p_out->_11 = (p_left->_01 * p_right->_10) + (p_left->_11 * p_right->_11) + (p_left->_21 * p_right->_12) + (p_left->_31 * p_right->_13);
-	p_out->_12 = (p_left->_02 * p_right->_10) + (p_left->_12 * p_right->_11) + (p_left->_22 * p_right->_12) + (p_left->_32 * p_right->_13);
-	p_out->_13 = (p_left->_03 * p_right->_10) + (p_left->_13 * p_right->_11) + (p_left->_23 * p_right->_12) + (p_left->_33 * p_right->_13);
-
-	p_out->_20 = (p_left->_00 * p_right->_20) + (p_left->_10 * p_right->_21) + (p_left->_20 * p_right->_22) + (p_left->_30 * p_right->_23);
-	p_out->_21 = (p_left->_01 * p_right->_20) + (p_left->_11 * p_right->_21) + (p_left->_21 * p_right->_22) + (p_left->_31 * p_right->_23);
-	p_out->_22 = (p_left->_02 * p_right->_20) + (p_left->_12 * p_right->_21) + (p_left->_22 * p_right->_22) + (p_left->_32 * p_right->_23);
-	p_out->_23 = (p_left->_03 * p_right->_20) + (p_left->_13 * p_right->_21) + (p_left->_23 * p_right->_22) + (p_left->_33 * p_right->_23);
-
-	p_out->_30 = (p_left->_00 * p_right->_30) + (p_left->_10 * p_right->_31) + (p_left->_20 * p_right->_32) + (p_left->_30 * p_right->_33);
-	p_out->_31 = (p_left->_01 * p_right->_30) + (p_left->_11 * p_right->_31) + (p_left->_21 * p_right->_32) + (p_left->_31 * p_right->_33);
-	p_out->_32 = (p_left->_02 * p_right->_30) + (p_left->_12 * p_right->_31) + (p_left->_22 * p_right->_32) + (p_left->_32 * p_right->_33);
-	p_out->_33 = (p_left->_03 * p_right->_30) + (p_left->_13 * p_right->_31) + (p_left->_23 * p_right->_32) + (p_left->_33 * p_right->_33);
+	__m128 a_line, b_line, r_line;
+	for (int i = 0; i < 16; i += 4) {
+		// unroll the first step of the loop to avoid having to initialize r_line to zero
+		a_line = _mm_load_ps(p_left->Points);         // a_line = vec4(column(a, 0))
+		b_line = _mm_set_ps1(p_right->Points[i]);      // b_line = vec4(b[i][0])
+		r_line = _mm_mul_ps(a_line, b_line); // r_line = a_line * b_line
+		for (int j = 1; j < 4; j++) {
+			a_line = _mm_load_ps(&p_left->Points[j * 4]); // a_line = vec4(column(a, j))
+			b_line = _mm_set_ps1(p_right->Points[i + j]);  // b_line = vec4(b[i][j])
+										   // r_line += a_line * b_line
+			r_line = _mm_add_ps(_mm_mul_ps(a_line, b_line), r_line);
+		}
+		_mm_store_ps(&p_out->Points[i], r_line);     // r[i] = r_line
+	}
 };
 
 void Mat_Mul_M4F_V4F(const Matrix4f_PTR p_left, const Vector4f_PTR p_right, Vector4f_PTR p_out)
 {
-	Mat_Mul_MXxXf_MXxXf((const char*)p_left, (const char*)p_right, 4, 1, sizeof(Vector4f), sizeof(Vector4f), (char*)p_out);
+	__m128 l_right = _mm_load_ps1(&p_right->Points[0]);
+	__m128 l_leftColum = _mm_load_ps(&p_left->Points[0]);
+	__m128 l_out = _mm_mul_ps(l_leftColum, l_right);
+	for (short int i = 1; i < 4; i++)
+	{
+		l_right = _mm_load_ps1(&p_right->Points[i]);
+		l_leftColum = _mm_load_ps(&p_left->Points[i * 4]);
+		l_out = _mm_add_ps(l_out, _mm_mul_ps(l_leftColum, l_right));
+		// l_left = _mm_load_ps(&p_left->Points[i * 4]);
+	}
+	_mm_store_ps(&p_out->Points[0], l_out);
 };
 
 void Mat_Mul_M4F_V4F_Homogeneous(const Matrix4f_PTR p_projectionmatrix, const Vector4f_PTR p_pos, Vector4f_PTR out_pos)
